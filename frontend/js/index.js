e0e93a2f741c53cf4d75c963699d26c8cf6a87bb
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2022-10-14 10:52:26 -0300

    Update index.js

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,6 +1,6 @@
 import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js';
 
-const socket = io('ws://talkware.velloware.com', {
+const socket = io('talkware.velloware.com/', {
   path: '/server'
 });
 
