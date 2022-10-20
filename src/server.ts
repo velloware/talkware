99c5bf7a0cf9dc54127e8c6a3feb4fa0a13a4921
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2022-10-20 20:12:27 -0300

    Update server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import Server from './infra/http/server';
 import { WebSocketServer } from './infra/ws/server';
 
-const server = new Server(3333, false);
+const server = new Server(5337, false);
 
 server.adpter(
   new WebSocketServer()
