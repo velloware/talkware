a390a0915851c152c8d1d6a29dbc9a02f581c7a1
Gabriel Barros Feitosa Sá <gabreilbarros13@gmail.com>
2022-12-26 22:11:56 -0300

    Docs: Put

--- src/infra/http/routes/docs/room/index.ts
+++ src/infra/http/routes/docs/room/index.ts
@@ -1,5 +1,9 @@
 import createRoom from './createRoom.json';
+import editRoom from './editRoom.json';
 
 export const roomPaths = {
-  '/rooms': createRoom,
+  '/rooms': {
+    ...createRoom,
+    ...editRoom,
+  },
 };
