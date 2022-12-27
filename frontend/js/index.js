e07b7bf70133e8743438f8b9c11f3063500560a7
Pedro Augusto Pereira Florentino <pedropereiraflorent@hotmail.com>
2022-12-27 15:12:32 -0300

    ideia de inteface frontend

--- frontend/js/index.js
+++ frontend/js/index.js
@@ -1,14 +1,14 @@
-/* eslint-disable no-undef */
 import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js';
 
 let tokenUser = 'Anonymous';
-// tokenUser = prompt(
-//   'Enter your tokenUser, IF you dont have one, please enter Anonymous or press Ok to continue',
-// );
-
-// tokenUser = tokenUser || 'Anonymous';
-
 let socket;
+
+const messageElement = document.querySelector('#message');
+const roomElement = document.querySelector('#room');
+const roomsPublic = document.querySelector('#roomsPublic');
+const roomId = document.querySelector('#roomIdJoin');
+const name = document.getElementById('name');
+
 if (
   window.location.hostname === 'localhost' ||
   window.location.hostname === '127.0.0.1'
@@ -28,59 +28,48 @@ if (
 
 socket.on('connect', () => setUserName(socket.id));
 socket.on('disconnect', () => console.log(`Disconnect For SocketServer`));
-
 socket.on('error', data => onError(data));
 socket.on('message', data => writeMessagesInteTextArea(data));
 socket.on('log', data => console.log(data));
-
 socket.on('voice', data => console.log(data));
 
-document.onkeyup = e => {
-  if (e.key === 'Enter') {
-    sendMessage();
-  }
-
-  if (e.key === 'Escape') {
-    document.getElementById('message').value = '';
-    document.getElementById('room').value = '';
-    socket.emit('leaveRoom');
-    setRoomName('No Room Selected. GLOBAL CHAT');
-    setUserName('No User Selected');
-  }
-};
-
 const sendMessage = () => {
-  const message = document.getElementById('message').value;
+  const message = messageElement.value;
+
+  if (message.trim() === '') return;
+
   writeMessagesInteTextArea(`Me: ${message}`);
+
   socket.emit('message', message);
-  document.getElementById('message').value = '';
+  messageElement.value = '';
 };
 
 const setUser = () => {
-  const name = document.getElementById('name').value;
-  socket.emit('changeName', name);
 
-  setUserName(`${name} With Session ${socket.id}`);
+  if (name.value.trim() === '') return;
+
+  socket.emit('changeName', name.value);
+
+  setUserName(`${name.value} With Session ${socket.id}`);
 };
 
-const writeMessagesInteTextArea = data => {
-  const roomRef = document.getElementById('room');
-  roomRef.value = roomRef.value += data += '\n';
+const writeMessagesInteTextArea = (data) => {
+  roomElement.value = roomElement.value += data += '\n';
 };
 
-const setUserName = name => {
+const setUserName = (name) => {
   const userIdRef = document.getElementById('userId');
   userIdRef.textContent = name;
 };
 
-const setRoomName = name => {
+const setRoomName = (name) => {
   const userIdRef = document.getElementById('roomCurrent');
   userIdRef.textContent = name;
 };
-setRoomName('No Room Selected. GLOBAL CHAT');
 
-const setRoom = idRoom => {
-  if (idRoom) {
+const setRoom = (idRoom) => {
+  
+  if (idRoom && typeof idRoom == 'string') {
     socket.emit('joinRoom', {
       idRoom: idRoom,
       token: 'Anonymous',
@@ -88,25 +77,50 @@ const setRoom = idRoom => {
 
     setRoomName(`${idRoom}`);
   } else {
-    const roomId = document.getElementById('roomIdJoin').value;
+
+    if (roomId.value.trim() === '') return;
+
     socket.emit('joinRoom', {
-      idRoom: roomId,
+      idRoom: roomId.value,
       token: 'Anonymous',
     });
 
-    setRoomName(`${roomId}`);
+    setRoomName(`${roomId.value}`);
   }
 };
 
 const chooseRoom = () => {
-  setRoom(document.getElementById('roomsPublic').value);
+  setRoom(roomsPublic.value);
 };
 
 const onError = error => {
   console.log(error);
 };
 
+const cleanElements = () => {
+  messageElement.value = '';
+  roomElement.value = '';
+  roomsPublic.options[0].selected = true;
+  roomId.value = '';
+  name.value = '';
+} 
+
 document.getElementById('sender').addEventListener('click', sendMessage);
 document.getElementById('setName').addEventListener('click', setUser);
 document.getElementById('setRoom').addEventListener('click', setRoom);
 document.getElementById('roomsPublic').addEventListener('change', chooseRoom);
+
+document.onkeyup = (e) => {
+  if (e.key === 'Enter') {
+    sendMessage();
+  }
+
+  if (e.key === 'Escape') {
+    cleanElements();
+    socket.emit('leaveRoom');
+    setRoomName('No Room Selected. GLOBAL CHAT');
+    setUserName('No User Selected');
+  }
+};
+
+setRoomName('No Room Selected. GLOBAL CHAT');
