606f967a605d506b9eaa11e7f3fc256f9cd2d732
Gabriel Barros Feitosa Sá <54006467+Colgate13@users.noreply.github.com>
2023-01-12 14:09:15 +0000

    Rudmenter Emmber js

--- frontend/js/staticChat.js
+++ frontend/js/staticChat.js
@@ -4,6 +4,13 @@ import { io } from 'https://cdn.socket.io/4.4.1/socket.io.esm.min.js';
 
 export class StaticChat {
   #idRoom = null;
+  #socket = null;
+  #chatArea = document.querySelector('#room');
+  #message = document.querySelector('#message');
+  #sender = document.querySelector('#sender');
+  #closeChat = document.querySelector('#closeChat');
+  #openChat = document.querySelector('#openChat');
+  #darkMode = document.querySelector('#darkMode');
 
   constructor(roomId) {
     this.#idRoom = roomId;
@@ -18,26 +25,36 @@ export class StaticChat {
   }
 
   initSocket() {
-    // const socket = io('https://talkware-backend.velloware.com', {
-    //   query: {
-    //     token: 'Anonymous',
-    //   },
-    // });
-    // socket.on('connect', () => {
-    //   console.log('Connected to server');
-    // });
-    // socket.on('disconnect', () => {
-    //   console.log('Disconnected from server');
-    // });
+    const socket = io('https://talkware-backend.velloware.com', {
+      query: {
+        token: 'Anonymous',
+      },
+    });
+    socket.on('connect', () => {
+      console.log('Connected to server');
+    });
+
+    socket.on('disconnect', () => {
+      console.log('Disconnected from server');
+    });
+
+    socket.emit('joinRoom', {
+      idRoom: '1',
+      token: 'Anonymous',
+    });
+
+    this.#socket = socket;
   }
 
   createElements() {
     const div = document.createElement('div');
     div.classList.add('chat');
     div.innerHTML = `
-    <div id="chatContainer">
-      <section class="section__chat">
+    <div class="chatContainer">
+    <button id="closeChat" type="button" class="button">X</button>
+    <section class="section__chat">
       <h1>Chat: Global Room 1</h1>
+      <h1>Session: Anonymous</h1>
       <textarea id="room" cols="30" rows="10" readonly></textarea>  
       <div>
         <input type="text" id="message"/>
@@ -46,7 +63,14 @@ export class StaticChat {
     </section>
   <div>`;
     document.body.appendChild(div);
-    return div;
+
+    const divOpenChat = document.createElement('div');
+    divOpenChat.classList.add('openChat');
+    divOpenChat.innerHTML = `
+    <div class="openChatContainer">
+      <button id="openChat" type="button" class="button">Open Chat</button>
+    </div>`;
+    document.body.appendChild(divOpenChat);
   }
 
   injectStyles() {
@@ -72,107 +96,100 @@ export class StaticChat {
       --bg-input: #2b2a33;
     }
 
-    #chatContainer {
+    .openChatContainer {
       position: fixed;
-      right: 0;
-      bottom: 10;
-      background: red;
-      width: 35%;
-      height: 100%;
-      align-items: center;
-      justify-content: center;
-    }
-    
-    #chatContainer .section__chat h1 {
-      color: var(--color-h2);
-      font-size: 1em;
-      margin-bottom: 0.5em;
-      z-index: 999;
-    }
-    
-    #chatContainer .section__chat textarea {
-      background: var(--bg-input);
-      color: var(--color-text);
-      border: none;
-      border-radius: 5px;
-      padding: 10px;
-      box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
-        rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-      z-index: 999;
-      margin-bottom: 100px;
-    }
-    
-    #chatContainer .section__chat textarea:focus {
-      outline: 0px solid transparent;
-      box-shadow: rgba(150, 160, 138, 1) 0px 5px 7px -1px,
-        rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-    }
-    
-    #chatContainer .section__chat button {
-      color: var(--color-button);
-      background: var(--bg-button);
-      border: none;
-      border-radius: 5px;
-      padding: 10px;
-      margin-top: 10px;
-      box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
-      z-index: 999;
-
+      top: 90%;
+      right: 20px;
+      font-size: 12px;
+      color: red;
+      padding: 10px;     
     }
 
-  #chatContainer .section__chat input,
-  textarea {
-    border-radius: 5px;
-    padding: 10px;
-    border: none;
-    color: var(--color-text);
-    background-color: var(--bg-input);
-    box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
+    .chatContainer {
+      position: fixed;
+      top: 45%;
+      right: 20px;
+      font-size: 12px;
+      color: red;
+      background-color: var(--bg);
+      box-shadow: rgba(50, 50, 93, 0.25) 0px 13px 27px -5px,
       rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-  }
-  
-  #chatContainer .section__chat input:focus,
-  textarea:focus {
-    outline: 0px solid transparent;
-    box-shadow: rgba(150, 160, 138, 1) 0px 5px 7px -1px,
-      rgba(0, 0, 0, 0.3) 0px 8px 16px -8px;
-  }
-  
-    
-  #chatContainer .section__chat {
-      padding-top: 2em;
-      text-align: center;
-    }
-    
-    #chatContainer .section__chat textarea {
-      width: 80%;
-      height: 60%;
-      margin-bottom: 20px;
-      resize: none;
-    }
-    
-    #chatContainer .section__chat div {
-      display: flex;
-      justify-content: center;
-    }
-    
-    #chatContainer .section__chat div input {
-      width: 60%;
-      height: 30px;
-    }
-    
-    #chatContainer .section__chat div button {
-      margin-left: 4%;
-      width: 16%;
+      padding: 10px;
     }`;
 
     document.head.appendChild(style);
+  }
+
+  writeMessagesInteTextArea(data) {
+    this.#chatArea = document.querySelector('#room');
+
+    this.#chatArea.value = this.#chatArea.value += data += '\n';
+  }
+
+  sendMessage(message) {
+    this.#socket.emit('message', message);
+  }
+
+  initSocketListeners() {
+    this.#socket.on('message', message => {
+      console.log(message);
+      this.writeMessagesInteTextArea(message);
+    });
+
+    this.#socket.on('roomData', data => {
+      console.log(data);
+    });
+  }
+
+  initListeners() {
+    const openChat = document.querySelector('#openChat');
+    openChat.addEventListener('click', () => {
+      const chat = document.querySelector('.chatContainer');
+
+      if (chat.style.display === 'block') {
+        chat.style.display = 'none';
+        return;
+      } else {
+        chat.style.display = 'block';
+        openChat.style.display = 'none';
+      }
+    });
+
+    const closeChat = document.querySelector('#closeChat');
+    closeChat.addEventListener('click', () => {
+      const chat = document.querySelector('.chatContainer');
+
+      if (chat.style.display === 'block') {
+        chat.style.display = 'none';
+        openChat.style.display = 'block';
+      } else {
+        chat.style.display = 'block';
+        openChat.style.display = 'none';
+      }
+    });
+
+    const sender = document.querySelector('#sender');
+    sender.addEventListener('click', () => {
+      const message = document.querySelector('#message').value;
+      const room = document.querySelector('#room');
+      room.innerHTML = message;
+      this.writeMessagesInteTextArea(`Me: ${message}`);
+      this.sendMessage(message);
+    });
+  }
+
+  hiddenChat() {
+    const chat = document.querySelector('.chatContainer');
+    chat.style.display = 'none';
   }
 
   build() {
     this.initSocket();
     this.createElements();
     this.injectStyles();
+    this.hiddenChat();
+    this.initSocketListeners();
+    this.initListeners();
   }
 }
 
