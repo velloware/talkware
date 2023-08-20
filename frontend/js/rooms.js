e315a1666d171f9e3c0c08df5c10735e194811a0
Colgate13 <gabreilbarros13@gmail.com>
2023-08-20 17:51:15 -0300

    fix(listRooms): http error in cache get

--- frontend/js/rooms.js
+++ frontend/js/rooms.js
@@ -2,12 +2,6 @@
 import { config } from './config.js';
 
 export async function getUserRooms() {
-  // if (
-  //   !localStorage.getItem('@token') ||
-  //   localStorage.getItem('@token') == 'Anonymous'
-  // )
-  //   return false;
-
   const response = await fetch(`${config.URLBACKEND}/rooms`, {
     method: 'GET',
     headers: {
@@ -15,8 +9,6 @@ export async function getUserRooms() {
       Authorization: 'Bearer ' + localStorage.getItem('@token'),
     },
   });
-
-  if (response.status != 200) return false;
 
   const rooms = await response.json();
 
